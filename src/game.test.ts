cb433ccf46bdf8c7aee48708fbc3589b8257a0d3
RickyJam <Albicocca35>
2021-09-11 17:18:48 +0200

    modificati test per injection

--- src/game.test.ts
+++ src/game.test.ts
@@ -1,7 +1,16 @@
 import Game from "./game";
+import {Generic, Fizz, Buzz} from './rules/RuleLibrary';
+import Node from './node/Node';
 
 describe('Game', () => {
-    const game = new Game();
+    const thirdNode = new Node(undefined, new Generic());
+    const secondNode = new Node(thirdNode, new Fizz());
+    const firstNode = new Node(secondNode, new Buzz());
+    const game = new Game(firstNode);
+
+    it('says value when it does not match any specific rule', ()=> {
+        expect(game.says(94)).toBe('94');
+    });
 
     it('says value when it does not match any specific rule', ()=> {
         expect(game.says(94)).toBe('94');
