1d84b0ab923a61fa8dbbed47ff0e9dd9010560bb
RickyJam <Albicocca35>
2021-09-11 16:49:11 +0200

    aggiunto test

--- src/node/Node.test.ts
+++ src/node/Node.test.ts
@@ -1,19 +1,26 @@
 import Node from "./Node";
+import Fizz from '../rules/fizz/Fizz';
 import Generic from '../rules/generic/Generic';
 
 
-
 describe('Node', () => {
-    const rule = new Generic();
+    const fizz = new Fizz();
+    const generic = new Generic();
 
     beforeAll(() => {
 
     });
 
     it('return value if rule is valid', () => {
-        const node = new Node(undefined, rule);
+        const node = new Node(undefined, generic);
 
         expect(node.apply(1)).toBe("1");
+    });
+
+    it('return value if rule is valid', () => {
+        const node = new Node(undefined, fizz);
+
+        expect(node.apply(3)).toBe("FIZZ");
     });
 
 });
